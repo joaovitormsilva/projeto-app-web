e9b00f637de384e532ac2aa17fe13a9567706f6f
PTurquetti <pauloturquetti29@gmail.com>
2024-06-10 11:58:11 -0300

    Correcao de erros no merge

--- projetoSqueezy/app/index.tsx
+++ projetoSqueezy/app/index.tsx
@@ -11,11 +11,6 @@ export default function SquizzyScreen() {
     return <ActivityIndicator size="large" color="#FCC307" />;
   }
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 4b252bb2dde39eb8d7870f1b96c3561631c87230
   return (
     <View style={styles.container}>
       <Image source={require('../assets/images/SquizzyLogo3.png')} style={styles.reactLogo} />
@@ -23,20 +18,12 @@ export default function SquizzyScreen() {
       <Text style={styles.text}>Your favorite quiz app</Text>
       <Text style={styles.text}>Create an account to start squizzying!</Text>
 
-<<<<<<< HEAD
-
-      <TouchableOpacity style={[styles.box, styles.BoxCreateAccount, { marginBottom: '10%', marginTop: '20%' }]}>
-        <Link href={"/CreateAccount"} style={styles.fullAreaLink}>
-            <Text style={[styles.buttonText, { color: "#05203C" }]}>Create Account</Text>
-        </Link>
-=======
       <TouchableOpacity
         style={[styles.box, styles.boxCreateAccount, { marginBottom: '10%', marginTop: '20%' }]}
         onPress={() => router.push('/CreateAccount')}
         accessibilityLabel="Create Account"
       >
         <Text style={[styles.text, styles.linkText]}>Create Account</Text>
->>>>>>> 4b252bb2dde39eb8d7870f1b96c3561631c87230
       </TouchableOpacity>
 
       <Text style={styles.text}>Already have an account?</Text>
@@ -70,18 +57,8 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontSize: 18,
-<<<<<<< HEAD
-    fontFamily: "Poppins_300Light",
-    textAlign: 'center',
-  },
-  buttonText: {
-      fontSize: 18,
-      fontFamily: "Poppins_300Light",
-      textAlign: 'center',
-=======
     fontFamily: 'Poppins_300Light',
     textAlign: 'center',
->>>>>>> 4b252bb2dde39eb8d7870f1b96c3561631c87230
   },
   linkText: {
     color: '#05203C',
@@ -89,20 +66,6 @@ const styles = StyleSheet.create({
   boxCreateAccount: {
     backgroundColor: '#FCC307',
   },
-<<<<<<< HEAD
-  fullAreaLink: {
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-
-  },
-  reactLogo: {
-    height: '40%',
-    width: '60%',
-
-=======
   boxSignIn: {
     backgroundColor: '#FFFFFF',
   },
@@ -110,6 +73,5 @@ const styles = StyleSheet.create({
     height: '40%',
     width: '70%',
     resizeMode: 'contain',
->>>>>>> 4b252bb2dde39eb8d7870f1b96c3561631c87230
   },
 });
